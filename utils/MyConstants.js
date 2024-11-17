69b4d2529ea9442c9569cd7e36d1bcbe75bc62e3
tringuyen <nguyenhuunhantri2003@gmail.com>
2024-11-17 10:12:02 +0700

    first commit

---
+++ utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'shopping.uqb2r.mongodb.net',
+    DB_USER: 'admin',
+    DB_PASS: '123',
+    DB_DATABASE: 'shopping',
+    EMAIL_USER: 'tri.2174802010236@hotmail.com', // Microsoft mail service
+    EMAIL_PASS: 'bovippro123',
+    JWT_SECRET: '<jwt_secret>',
+    JWT_EXPIRES: '99999999999', // in milliseconds
+};
+module.exports = MyConstants;
